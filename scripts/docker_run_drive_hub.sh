ea5a8fe627f1572d1e7992f677042bffc97bd836
avivdean <deanaviv@gmail.com>
2024-09-22 21:13:37 +0800

    enable display forwarding from docker to host

--- scripts/docker_run_drive_hub.sh
+++ scripts/docker_run_drive_hub.sh
@@ -11,6 +11,10 @@ CYAN_BOLD='\033[1;36m'
 docker_image=drive_hub
 echo -e "${RED}Launching docker image ${RED_BOLD}$docker_image"${NC}
 echo $PWD
+
+echo -e "${GREEN}Setting X11 permissions...${NC}"
+xhost +local:docker
+
 #  -v /home/nvidia/git/ros2_workspace:/workspaces/ros2_workspace \
 run_docker_cmd="docker run \
   --name drive_hub \
@@ -27,7 +31,10 @@ run_docker_cmd="docker run \
   --cap-add=CAP_SYS_RESOURCE \
   -e ROS_DOMAIN_ID=1 \
   -e PYTHONOPTIMIZE=2 \
-   "
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -e XDG_RUNTIME_DIR=/tmp/xdg \
+  "
 run_docker_cmd+=$docker_image 
 echo -e "${CYAN_BOLD}$run_docker_cmd${NC}"
 eval "$run_docker_cmd"
