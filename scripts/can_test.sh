5c9b54ac0afbb9953ca644f4b7c5c168b922e5e1
avivdean <deanaviv@gmail.com>
2025-01-06 22:14:46 +0800

    update can_test

--- scripts/can_test.sh
+++ scripts/can_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 send_can_drive_msgs() {
-    end_time=$((SECONDS + 5))
+    end_time=$((SECONDS + 30))
 
     while [ $SECONDS -lt $end_time ]; do
-        cansend can0 203#015200
-        cansend can0 204#01ADFE
+        cansend can0 203#018B00
+        #cansend can0 204#01ADFE
     done
 
     cansend can0 203#01000000
